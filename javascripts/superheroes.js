994689ad22939a31f6a0cb51e776f8cf29556ec8
April Nichols <nicholsar@gmail.com>
2018-04-02 21:23:01 -0500

    hero cards started

--- javascripts/superheroes.js
+++ javascripts/superheroes.js
@@ -1,1 +1,32 @@
-console.log('heroes');
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML += domString;
+}
+
+const buildDomString = (heroArray) => {
+    let heroCard = '';
+    heroArray.forEach((hero) => {
+        heroCard += `<div class="hero-card">`;
+        heroCard +=     `<h2>${hero.name}</h2>`;
+        heroCard += `</div>`;
+    });
+    printToDom(heroCard, 'hero-container');
+}
+
+function executeOnLoad() {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheroes);
+}
+
+function executeIfFail () {
+    console.log('Something went wrong.');
+}
+
+const startApp = () => {
+    let myRequest = new XMLHttpRequest();
+    myRequest.addEventListener('load', executeOnLoad);
+    myRequest.addEventListener('error', executeIfFail);
+    myRequest.open("GET", "../db/superheroes.json");
+    myRequest.send();
+}
+
+startApp();
